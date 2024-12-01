d68afd9f16a1f16ef7efe7a3c9390c15a4edd898
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-12-01 20:11:04 +0200

    chore: migrate to com.iqkv.boot.web.rest package name

--- src/main/java/com/iqkv/incubator/quickstart/userprofilecrud/web/UserProfileResource.java
+++ src/main/java/com/iqkv/incubator/quickstart/userprofilecrud/web/UserProfileResource.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.iqkv.boot.web.rest.ApiError;
+import com.iqkv.boot.mvc.rest.ApiError;
 import com.iqkv.incubator.quickstart.userprofilecrud.entity.UserProfile;
 import com.iqkv.incubator.quickstart.userprofilecrud.repository.UserProfileRepository;
 import io.swagger.v3.oas.annotations.Operation;
