ea9736bd7cd0106d8d3c36763016f4446c9d446f
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-01-30 19:09:47 +0200

    chore: update streamline version and copyright headers

--- src/main/java/com/iqkv/incubator/quickstart/userprofilecrud/config/ApplicationConfig.java
+++ src/main/java/com/iqkv/incubator/quickstart/userprofilecrud/config/ApplicationConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 IQKV Team.
+ * Copyright 2025 IQKV Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories({"com.iqkv.incubator.quickstart.userprofilecrud.repository"})
 @EnableJpaAuditing
 @EnableTransactionManagement
-@OpenAPIDefinition(info = @Info(title = "User Profile Management", version = "24.0.0"))
+@OpenAPIDefinition(info = @Info(title = "User Profile Management", version = "25.0.0"))
 class ApplicationConfig {
 
 }
